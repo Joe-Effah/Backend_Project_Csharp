04175dbd20b049a162f2260389b32544a828930d
Joe <124173235+Joe-Effah@users.noreply.github.com>
2023-04-26 05:03:38 -0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ContructorFinder.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
