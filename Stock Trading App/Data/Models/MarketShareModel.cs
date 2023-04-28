1a5a3dbebc0e71d742c9c315c0168e1ed8a6b333
Joe <124173235+Joe-Effah@users.noreply.github.com>
2023-04-28 08:54:35 -0700

    Add files via upload

---
+++ Stock Trading App/Data/Models/MarketShareModel.cs
@@ -0,0 +1,24 @@
+
+
+namespace Stock_Trading_App.Data.Models;
+
+
+
+public class MarketData
+{
+public string? market_type  {get;set;}	
+public string? Counrty	{get;set;}	
+public string? primary_exchanges	 {get;set;}	
+public DateTime local_open	{get;set;}	
+public DateTime local_close	{get;set;}	
+public string? current_status	{get;set;}	
+public string? notes	{get;set;}	
+    
+}
+
+public class MarketShareModel
+{
+public string? api_Message { get; set; }
+
+public List<List<MarketData>>?  listOfMarketData{get;set;}
+}
