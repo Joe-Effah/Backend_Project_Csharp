f12955c87c5920f3a00e47fd0eb74b7358a3456f
Joe <124173235+Joe-Effah@users.noreply.github.com>
2023-04-30 09:24:32 -0700

    Add files via upload

---
+++ Stock Trading App/Data/CurrencyService.cs
@@ -0,0 +1,21 @@
+
+using Stock_Trading_App.Data.Models;
+using Stock_Trading_App.Services;
+
+namespace Stock_Trading_App.Data;
+
+
+class CurrencyService : ICurrencyServices
+{
+    public CurrencyModel ConvertCryptoCurrency(string CurrencyFrom, string CurrencyTo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CurrencyModel ConvertForexCurrency(string CurrencyFrom, string CurrencyTo)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+ 
